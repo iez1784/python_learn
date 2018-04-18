41ba343a377b5fe0ace9dfd1476de6c93df24c3a
iez17 <edisonz520@gmail.com.cn>
2018-04-18 23:26:25 +0800

    add PythonCrashCourse

---
+++ PythonCrashCourse/Charpter_3_List/charpter_3_List.py
@@ -0,0 +1,74 @@
+__author__ = 'zhangedison'
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#访问列表元素
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+
+print(bicycles[3])
+
+print(bicycles[-1])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorycles_new = []
+print(motorycles_new)
+motorycles_new.append('honda_new')
+motorycles_new.append('yamaha_new')
+motorycles_new.append('suzuki_new')
+print(motorycles_new)
+
+motorycles_insert = ['honda_1', 'yamaha_2', 'suzuki_3']
+motorycles_insert.insert(0, 'ducati_1')
+print(motorycles_insert)
+
+del motorycles_insert[0]
+print(motorycles_insert)
+
+
+motorycles_pop = ['honda_pop', 'yamaha_pop', 'suzuki_pop']
+print(motorycles_pop)
+
+popped_motorycles = motorycles_pop.pop()
+print(motorycles_pop)
+print(popped_motorycles)
+
+motorycles_pop = ['honda_pop', 'yamaha_pop', 'suzuki_pop']
+last_owned = motorycles_pop.pop()
+print("The last motorycle I owned was a " + last_owned.title() + ".")
+
+
+motorycles_pop = ['honda_pop', 'yamaha_pop', 'suzuki_pop']
+first_owned = motorycles_pop.pop(0)
+print("The first motorycle I owned was a " + first_owned.title() + ".")
+
+
+motorycles_remove = ['honda_remove', 'yamaha_remove', 'suzuki_remove', 'ducati_remove']
+print(motorycles_remove)
+motorycles_remove.remove('ducati_remove')
+print(motorycles_remove)
+
+motorycles_remove = ['honda_remove', 'yamaha_remove', 'suzuki_remove', 'ducati_remove']
+print(motorycles_remove)
+
+too_expensive = 'ducati_remove'
+motorycles_remove.remove(too_expensive)
+print(motorycles_remove)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
---
+++ PythonCrashCourse/helloword.py
@@ -0,0 +1,53 @@
+__author__ = 'zhangedison'
+
+
+print('hello world')
+
+
+message = 'Hello Python world!'
+
+print(message)
+
+message = 'Hello Python Crash Course world'
+
+print(message)
+
+print("The language 'Python' is named after Monty Python, not the snake.")
+
+name = 'ada lovelace'
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'edison'
+last_name = 'lovelace'
+full_name = first_name + " " + last_name
+print(full_name)
+
+print('Hello, ' + full_name.title() + " !")
+
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+
+
+people_name = 'Eric'
+
+print("hello" + " " + people_name + ", would you like to learn some Python today?")
+
+#########################################################################################
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0].title())
+
+message2 = "My first bicycle was a " + bicycles[0].title() + "."
+
+print(message2)
