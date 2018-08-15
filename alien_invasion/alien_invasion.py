406e278930da34bb12645b80967e7ce8d8b01e24
Edison.W.Zhang <edison.w.zhang@newegg.com>
2018-08-15 19:02:23 +0800

    add demo

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from  ship import Ship
 import game_functions as gf
+from alien import Alien
 from pygame.sprite import Group
 
 
@@ -11,15 +12,17 @@ def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_heigh))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
 
-   #创建一般飞船
+   #创建一般飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-
-    #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
     # 开始游戏的主循环
@@ -27,9 +30,10 @@ def run_game():
         #监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings,aliens)
         #每次循环时都重绘屏幕
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,7 +7,7 @@ class Settings():
         """初始化游戏的设置"""
         # 屏幕设置
         self.screen_width = 1200
-        self.screen_heigh = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         #飞船的设置
@@ -18,3 +18,10 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移， 为-1表示向左移
+        self.fleet_direction = 1
